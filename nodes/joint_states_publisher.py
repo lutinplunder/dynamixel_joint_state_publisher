2ef65153a2aaadf165dc14d1f95cffc553fd5526
Tam, Benjamin (Data61, Pullenvale) <Benjamin.Tam@csiro.au>
2020-05-08 18:41:31 +1000

    Added gitignore and license - updated package format

--- nodes/joint_states_publisher.py
+++ nodes/joint_states_publisher.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+#######################################################################
+# Copyright (c) 2020
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230
+#######################################################################
+
 import rospy
 from dynamixel_msgs.msg import JointState as DynamixelJointState
 from std_msgs.msg import *
